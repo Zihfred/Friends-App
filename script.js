2b2e7ff89f2bcff035f3a8de2b657152432b4b94
Zihfred <lichkingblizzard@gmail.com>
2018-12-18 22:38:19 +0200

    fixed script

--- script.js
+++ script.js
@@ -1,25 +1,30 @@
 class FriendApp {
-    constructor(){
+    constructor() {
         this.sortBox = document.querySelector('.sortBox');
         this.peoples = document.querySelector('.peoples');
         this.resetBtn = document.querySelector('.sortBox__resetBtn');
         this.sortInput = document.querySelector('.sortBox__sortedInput');
+        this.sortIconMale = document.querySelector('.fa-male');
+        this.sortIconFemale = document.querySelector('.fa-female');
         this.data = [];
         this.startData = [];
+
         this.init();
 
         this.resetPeople = this.resetPeople.bind(this);
         this.sort = this.sort.bind(this);
-        this.sortByInput = this.sortByInput.bind(this);
+        this.renderSortedByInput = this.renderSortedByInput.bind(this);
         this.addEventsListeners();
     }
-    init(){
-        //this.addCardToPeoples(this.generateOnePeopleCard());
-        fetch('https://randomuser.me/api/?results=100',{
+
+    init() {
+        this.getResFromServer();
+    }
+    getResFromServer(){
+        fetch('https://randomuser.me/api/?results=100', {
             method: "GET"
         })
             .then(res => {
-
                 return res.json();
             })
             .then(res => {
@@ -31,8 +36,7 @@ class FriendApp {
                 console.log(err);
             })
     }
-    generatePeoples(data){
-
+    generatePeoples(data) {
         data.forEach(onePeople => {
             this.addCardToPeoples(this.generateOnePeopleCard(onePeople));
         })
@@ -43,15 +47,18 @@ class FriendApp {
     }
 
 
-    resetPeople(){
+    resetPeople() {
+        this.sortIconFemale.classList.remove('disabled');
+        this.sortIconFemale.classList.remove('nonActive');
+        this.sortIconMale.classList.remove('disabled');
+        this.sortIconMale.classList.remove('nonActive');
         this.sortInput.value = '';
         this.clearPeoplesForm();
         this.data = this.startData;
         this.generatePeoples(this.data);
     }
 
-
-    generateOnePeopleCard(obj){
+    generateOnePeopleCard(obj) {
         let onePeople = document.createElement('div');
         onePeople.classList.add('onePeople');
 
@@ -92,116 +99,172 @@ class FriendApp {
         return onePeople;
     }
 
-    addCardToPeoples(card){
+    addCardToPeoples(card) {
         this.peoples.appendChild(card);
     }
 
-    clearPeoplesForm(){
+    clearPeoplesForm() {
         this.peoples.innerHTML = '';
     }
 
-
-
-    sort(e){
-        if(e.target.classList.contains('fas')){
+    sort(e) {
+        if (e.target.classList.contains('fas')) {
             switch (e.target.dataset.type) {
                 case "ageUp":
-
-                    this.clearPeoplesForm();
-                    console.log(this.data);
-                    this.generatePeoples(this.data.sort(function(a, b) {
-                        console.log()
-                        return a.dob.age - b.dob.age;
-                    }));
-
-
+                    this.filterByAgeUp();
                     break;
                 case "ageDown":
-
-                    this.clearPeoplesForm();
-                    console.log(this.data);
-                    this.generatePeoples(this.data.sort(function(a, b) {
-                        console.log()
-                        return b.dob.age - a.dob.age;
-                    }));
-
+                    this.filterByAgeDown();
                     break;
                 case "nameDown":
-
-                    this.clearPeoplesForm();
-                    this.generatePeoples(
-                    this.data.sort(function(a, b){
-                        if(a.name.first < b.name.first) { return -1; }
-                        if(a.name.first > b.name.first) { return 1; }
-                        return 0;
-                    }))
-
-
-
-
+                    this.filterByNameDown();
                     break;
                 case "nameUp":
-
-                    this.clearPeoplesForm();
-                    this.generatePeoples(
-                        this.data.sort(function(a, b){
-                            if(a.name.first < b.name.first) { return 1; }
-                            if(a.name.first > b.name.first) { return -1; }
-                            return 0;
-                        }))
-
+                        this.filterByNameUp();
                     break;
                 case "male":
-                    this.data = this.startData;
-                    this.clearPeoplesForm();
-                    this.data =  this.data.filter(function (people) {
-                        return people.gender === 'male';
-                    })
-                    this.generatePeoples(this.data);
-
+                       this.filterByMale();
 
                     break;
                 case "female":
-                    this.data = this.startData;
-                    this.clearPeoplesForm();
-                    this.data = this.data.filter(function (people) {
-                        return people.gender === 'female';
-                    });
-                    this.generatePeoples( this.data);
-
-                    break;
-
+                   this.filterByFemale();
             }
         }
     }
+    filterByAgeUp(){
+        this.clearPeoplesForm();
+         let res;
+        if(this.sortInput.value){
+            res =  this.filterByInputAll();
+        }
+        res = this.data.sort(function (a, b) {
+            return a.dob.age - b.dob.age;
+        });
+        this.generatePeoples(res);
 
-    sortByInput() {
-        this.data = this.startData;
+    }
+    filterByAgeDown(){
+        let res;
+        if(this.sortInput.value){
+            res =  this.filterByInputAll();
+        }
+        res = this.data.sort(function (a, b) {
+            return b.dob.age - a.dob.age;
+        });
+        this.clearPeoplesForm();
+        this.generatePeoples(res);
+
+
+    }
+    filterByNameDown(){
+        let res;
+        if(this.sortInput.value){
+            res =  this.filterByInputAll();
+        }
+        res = this.data.sort(function (a, b) {
+            if (a.name.first < b.name.first) {
+                return -1;
+            }
+            if (a.name.first > b.name.first) {
+                return 1;
+            }
+            return 0;
+        });
+        this.clearPeoplesForm();
+        this.generatePeoples(res);
+
+    }
+    filterByNameUp(){
+        let res;
+        this.clearPeoplesForm();
+        if(this.sortInput.value){
+            res = this.filterByInputAll();
+        }
+        res = this.data.sort(function (a, b) {
+            if (a.name.first < b.name.first) {
+                return 1;
+            }
+            if (a.name.first > b.name.first) {
+                return -1;
+            }
+            return 0;
+        });
+        this.clearPeoplesForm();
+        this.generatePeoples(res);
+
+
+    }
+    filterByMale(){
+        this.sortIconFemale.classList.add('disabled');
+        this.sortIconMale.classList.add('nonActive');
+        if(this.sortInput.value){
+            this.data = this.filterByInputAll('male');
+        }else{
+            this.data = this.startData;
+        }
+
+        this.data = this.data.filter(function (people) {
+            return people.gender === 'male';
+        })
+
+        this.clearPeoplesForm();
+        this.generatePeoples(this.data);
+    }
+    filterByFemale(){
+        this.sortIconMale.classList.add('disabled');
+        this.sortIconFemale.classList.add('nonActive');
+        if(this.sortInput.value){
+            this.data = this.filterByInputAll();
+        }else{
+            this.data = this.startData;
+        }
+
+        if(this.sortInput.value){
+            this.data = this.filterByInputAll('female');
+        }
+
+        this.data = this.data.filter(function (people) {
+            return people.gender === 'female';
+        })
+
+        this.clearPeoplesForm();
+        this.generatePeoples(this.data);
+    }
+    filterByInputAll(gender){
+        let result;
+
+            result = this.data.filter(function (people) {
+
+                return people.name.first.indexOf(this.sortInput.value) != -1 || people.name.last.indexOf(this.sortInput.value) != -1;
+            }.bind(this));
+        
+            return result;
+
+
+
+
+        return result;
+    }
+
+    renderSortedByInput() {
         if (!this.sortInput.value) {
+            this.data  = this.startData;
             this.clearPeoplesForm();
             this.generatePeoples(this.data);
         } else {
             this.clearPeoplesForm();
-            let inputValue = this.sortInput.value;
-            this.clearPeoplesForm();
-            this.data = this.data.filter(function (people) {
-                console.log(people.name.first);
-                return people.name.first.indexOf(inputValue) != -1 || people.name.last.indexOf(inputValue) != -1;
-            });
+            this.data = this.filterByInputAll();
             this.generatePeoples(this.data);
 
         }
-        console.log(this.data);
     }
 
+    addEventsListeners() {
+        this.sortInput.addEventListener('input', this.renderSortedByInput);
+        this.sortBox.addEventListener('click', this.sort);
+        this.resetBtn.addEventListener('click', this.resetPeople);
 
-    addEventsListeners(){
-        this.sortBox.addEventListener('click',this.sort);
-        this.resetBtn.addEventListener('click',this.resetPeople);
-        this.sortInput.addEventListener('input',this.sortByInput)
     };
-
-
 }
 
 new FriendApp();
